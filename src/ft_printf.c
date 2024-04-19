401d31956af2c3bdd2b242caefbd708d69092476
Julia Wolfram <julia.wolfram@gmx.at>
2024-04-19 12:10:34 +0200

    work: made basic prinft without %

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: jwolfram <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 18:21:25 by jwolfram          #+#    #+#             */
-/*   Updated: 2024/04/18 19:27:26 by jwolfram         ###   ########.fr       */
+/*   Updated: 2024/04/19 12:09:21 by jwolfram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,31 @@
 
 int	ft_printf(const char *format, ...)
 {
-	va_list		*arg;
-	va_start	*start;
+//	va_list arg;
+	int		len;
+	int		i;
+
+	i = 0;
+	len = 0;
+//	va_start(arg, format);
+	while (format[i])
+	{
+//		if (format[i] == '%')
+//		{}
+//		else
+		len += write(1, &format[i], 1);
+		i++;
+	}
+//	va_end(arg);
+	return (len);
+}
+
+#include <stdio.h>
+
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[1]);
+	ft_printf("world");
+	return (0);
 }
