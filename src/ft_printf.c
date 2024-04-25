d4c20efe406701e0eee5e97a5e83e317e2ebaa4a
Julia Wolfram <julia.wolfram@gmx.at>
2024-04-25 14:30:00 +0200

    refactor: added int bytes and STDOUT_FILENO

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: jwolfram <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 18:21:25 by jwolfram          #+#    #+#             */
-/*   Updated: 2024/04/24 18:38:55 by jwolfram         ###   ########.fr       */
+/*   Updated: 2024/04/25 14:28:52 by jwolfram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,10 @@
 
 int	print_char(char c)
 {
-	write(1, &c, 1);
+	int	bytes;
+
+	bytes = write(STDOUT_FILENO, &c, 1);
+	return (bytes);
 }
 
 void	print_str(char *str)
@@ -23,8 +26,6 @@ void	print_str(char *str)
 	int	i;
 
 	i = 0;
-	if (!str)
-		return ;
 	while (str[i])
 	{
 		write(1, &str[i], 1);
@@ -32,10 +33,15 @@ void	print_str(char *str)
 	}
 }
 
-int	ft_eval(va_list arg, const char *format)
+void	print_ptr(char *ptr)
+{
+	
+}
+
+static int	ft_eval(va_list arg, int format)
 {
 	if (format == 'c')
-		return (print_char(va_arg(arg, char)));
+		return (print_char(va_arg(arg, int)));
 	else if (format == 's')
 		return (print_str(va_arg(arg, char *)));
 	else if (format == 'p')
@@ -46,32 +52,34 @@ int	ft_eval(va_list arg, const char *format)
 		return (print_int(va_arg(arg, int)));
 	else if (format == 'u')
 		return (print_unsigned(va_arg(arg, unsigned int)));
-	else if (format == 'x')
-		return (print_hexlow(va_arg(arg, unsigned int)));
-	else if (format == 'X')
-		return (print_hexup(va_arg(arg, unsigned int)));
+	else if (format == 'X' || format == 'x')
+		return (print_hex(va_arg(arg, unsigned int)));
 	else if (format == '%')
-		write(1, '%', 1);
+		write(1, "%", 1);
+	return (-1);
 }
 
 int	ft_printf(const char *format, ...)
 {
 	va_list arg;
-	int		len;
-	int		i;
+	size_t	len;
+	size_t	i;
+	int		bytes;
 
 	i = 0;
 	len = 0;
+	if (!format)
+		return (-1);
 	va_start(arg, format);
-	while (format[i])
+	while (format && format[i])
 	{
-		if (format[i] == '%')
+		if (format[i] == '%' && format[i + 1])
 		{
 			ft_eval(arg, format[i + 1]);
 			i++;
 		}
 		else
-		len += write(1, &format[i], 1);
+			len += write(1, &format[i], 1);
 		i++;
 	}
 	va_end(arg);
@@ -84,6 +92,6 @@ int	main(int argc, char **argv)
 {
 	(void)argc;
 	printf("%s\n", argv[1]);
-	ft_printf("world");
+	ft_printf("%s", argv[1]);
 	return (0);
 }
