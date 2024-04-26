feea9cc068beb5a4fd10d360a147ccef9b6cd863
Julia Wolfram <julia.wolfram@gmx.at>
2024-04-26 16:04:37 +0200

    chore: added all used functions

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,17 +6,24 @@
 /*   By: jwolfram <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 10:47:33 by jwolfram          #+#    #+#             */
-/*   Updated: 2024/04/23 10:51:33 by jwolfram         ###   ########.fr       */
+/*   Updated: 2024/04/26 15:49:46 by jwolfram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include "libft/libft.h"
 # include <stdarg.h>
+# include <unistd.h>
 
 int	ft_printf(const char *, ...);
+int	ft_print_char(char c);
+int	ft_print_str(char *str);
+size_t	ft_strlen(const char *s);
+int	ft_print_nbr(int nbr);
+int	ft_print_ptr(void *ptr);
+int	ft_print_hex(unsigned long hex, int format);
+int	ft_print_unsigned(unsigned int nbr);
 
 #endif
 
