c5ee8125630b9ded126eb6ad998d6bf38a3403f1
Julia Wolfram <julia.wolfram@gmx.at>
2024-05-08 13:02:09 +0200

    final push

--- src/ft_print_hex.c
+++ src/ft_print_hex.c
@@ -6,7 +6,7 @@
 /*   By: jwolfram <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 15:31:23 by jwolfram          #+#    #+#             */
-/*   Updated: 2024/04/26 16:39:05 by jwolfram         ###   ########.fr       */
+/*   Updated: 2024/05/06 17:48:05 by jwolfram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ int	ft_print_hex(unsigned long hex, int format)
 	base = "0123456789abcdef";
 	if (format == 'X')
 		base = "0123456789ABCDEF";
+	bytes = 0;
 	if (hex < 16)
 	{
 		temp = ft_print_char(base[hex]);
@@ -29,7 +30,7 @@ int	ft_print_hex(unsigned long hex, int format)
 	temp = ft_print_hex(hex / 16, format);
 	if (temp == -1)
 		return (temp);
-	bytes = temp;
+	bytes += temp;
 	temp = ft_print_hex(hex % 16, format);
 	if (temp == -1)
 		return (temp);
